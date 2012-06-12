ad925d636115739d0b7f6d788f15a8c952546473
Dominik Borchardt <dominik.borchardt@mayflower.de>
2012-06-12 13:27:55 +0200

    Added About page

--- app/view/About.js
+++ app/view/About.js
@@ -5,7 +5,15 @@ Ext.define('ScrumButMobile.view.About', {
     config: {
         title: 'Impressum',
         iconCls: 'user',
-        html: 'Impressum',
+        html: '<b>Entwickler</b><br>'+
+			  'Dominik Borchardt <br>' +
+			  '<a href="mailto:zerocool89@online.de">Email</a><br><br>' +
+			  '<a href="https://github.com/I3re4k/ScrumButMobile">GitHub Repository</a> <br><br>' +
+			  '<b> In Kooperation mit: </b><br>' +
+			  '<a href="http://www.mayflower.de"> Mayflower GmbH </a><br><br>' +
+			  '<b>Scrum But Test</b> <br>' +
+			  '&copy; Jeff Sutherland',
+			  
         
         items: [
             {
